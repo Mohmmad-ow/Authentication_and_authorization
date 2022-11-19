7eb7b5f6a233ae44db19f677b1b8b29a987cf43f
Mohmmad-ow <moemessi00020@gmail.com>
2022-11-19 18:05:13 +0300

    basic express+mangodb session setup

--- sessions/app.js
+++ sessions/app.js
@@ -1,12 +1,45 @@
-const app = require("express")()
 const mongoose = require("mongoose")
 const session = require("express-session")
+const bodyParser = require("body-parser")
 
 
-
-mongoose.connect("mongodb://localhost:27017/test");
-
-const Cat = mongoose.model('Cat', {name: String});
+const MongoStore = require('connect-mongo');
 
 
-const kitty = new Cat({name: "my Cat"}).save()
+const app = require("express")()
+app.use(bodyParser.urlencoded({extended: true}))
+
+const dbString = 'mongodb://localhost:27017/test'
+const dbOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect('mongodb://localhost/test');
+
+app.use(session({
+    secret: "something secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongoUrl: 'mongodb://localhost/test',
+        mongoOptions: dbOptions
+     }),
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}))
+
+app.get('/', (req, res, next) => {
+    
+    if (req.session.visitCount) {
+        req.session.visitCount++
+    } else {
+        req.session.visitCount = 1
+    }
+    res.send(`you've visited this website for ${req.session.visitCount}`)
+})
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000!")
+})
